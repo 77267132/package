5a7ec52f8c59ac49dea74bb36750f3ed533a72c9
77267132 <155213818+77267132@users.noreply.github.com>
2024-01-06 15:43:10 +0800

    Update speedtestwan.lua

--- luci-app-netspeedtest/luasrc/model/cbi/netspeedtest/speedtestwan.lua
+++ luci-app-netspeedtest/luasrc/model/cbi/netspeedtest/speedtestwan.lua
@@ -2,7 +2,7 @@
 require("luci.util")
 local o,t,e
 
-o = Map("netspeedtest", "<font color='green'>" .. translate("Net Speedtest") .."</font>",translate( "Network speed diagnosis test (including intranet and extranet)<br/>For specific usage, see:") ..translate("<a href=\'https://github.com/sirpdboy/netspeedtest.git' target=\'_blank\'>GitHub @sirpdboy/netspeedtest</a>") )
+o = Map("netspeedtest", "<font color='green'>" .. translate("Net Speedtest") .."</font>",translate( "Network speed diagnosis test (including intranet and extranet)") )
 
 t=o:section(TypedSection,"wanspeedtest",translate("Wan Speedtest"))
 t.anonymous=true
