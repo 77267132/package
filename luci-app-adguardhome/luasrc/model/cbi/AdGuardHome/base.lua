b248e0f1d33ed271780878c4f95f7e323af7e8ca
77267132 <155213818+77267132@users.noreply.github.com>
2024-01-07 12:26:47 +0800

    Update base.lua

--- luci-app-adguardhome/luasrc/model/cbi/AdGuardHome/base.lua
+++ luci-app-adguardhome/luasrc/model/cbi/AdGuardHome/base.lua
@@ -24,7 +24,7 @@ o.placeholder=3000
 o.default=3000
 o.datatype="port"
 o.optional = false
-o.description = translate("<input type=\"button\" style=\"width:210px;border-color:Teal; text-align:center;font-weight:bold;color:Green;\" value=\"AdGuardHome Web:"..httpport.."\" onclick=\"window.open('http://'+window.location.hostname+':"..httpport.."/')\"/>")
+o.description = translate("<input type=\"button\" style=\"width:210px;border-color:Teal; text-align:center;font-weight:bold;color:Green;\" value=\"Open the management page\" onclick=\"window.open('http://'+window.location.hostname+':"..httpport.."/')\"/>")
 local binmtime=uci:get("AdGuardHome","AdGuardHome","binmtime") or "0"
 local e=""
 if not fs.access(configpath) then
